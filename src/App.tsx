d68338d2b76d9fe2c720b7bca0dac4831052c516
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2024-11-27 19:41:46 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+//File: App.tsx located in src/ directory
+import "./App.css";
+function App() {
+  return (
+    <div style={{ backgroundColor: "black", color: "white" }}>
+      <h1>This is main componenet (App.tsx)</h1>
+    </div>
+  );
+}
+
+export default App;
